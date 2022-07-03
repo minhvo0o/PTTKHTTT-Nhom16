3f528098cc6fd6c9f1be1ab36ef09878bf4e1e20
Minh Vo <66895221+minhvo0o@users.noreply.github.com>
2022-07-03 10:40:28 +0700

    TiemChung

---
+++ TiemChung/PTTKHTTT/capNhatPhieuDKTiem.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PTTKHTTT
+{
+    partial class capNhatPhieuDKTiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleCapNhat = new System.Windows.Forms.Label();
+            this.lblMaKH = new System.Windows.Forms.Label();
+            this.lblTgTiem = new System.Windows.Forms.Label();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.timeTgTiem = new System.Windows.Forms.DateTimePicker();
+            this.btnXacNhan = new System.Windows.Forms.Button();
+            this.chkDuocTiem = new System.Windows.Forms.CheckBox();
+            this.chkDaTiem = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // titleCapNhat
+            // 
+            this.titleCapNhat.AutoSize = true;
+            this.titleCapNhat.Font = new System.Drawing.Font("Segoe UI Black", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.titleCapNhat.Location = new System.Drawing.Point(58, 34);
+            this.titleCapNhat.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleCapNhat.Name = "titleCapNhat";
+            this.titleCapNhat.Size = new System.Drawing.Size(221, 30);
+            this.titleCapNhat.TabIndex = 0;
+            this.titleCapNhat.Text = "Phiếu đăng ký tiêm";
+            // 
+            // lblMaKH
+            // 
+            this.lblMaKH.AutoSize = true;
+            this.lblMaKH.Location = new System.Drawing.Point(58, 86);
+            this.lblMaKH.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMaKH.Name = "lblMaKH";
+            this.lblMaKH.Size = new System.Drawing.Size(89, 15);
+            this.lblMaKH.TabIndex = 1;
+            this.lblMaKH.Text = "Mã khách hàng";
+            // 
+            // lblTgTiem
+            // 
+            this.lblTgTiem.AutoSize = true;
+            this.lblTgTiem.Location = new System.Drawing.Point(58, 232);
+            this.lblTgTiem.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTgTiem.Name = "lblTgTiem";
+            this.lblTgTiem.Size = new System.Drawing.Size(83, 15);
+            this.lblTgTiem.TabIndex = 3;
+            this.lblTgTiem.Text = "Thời gian tiêm";
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Enabled = false;
+            this.txtMaKH.Location = new System.Drawing.Point(58, 111);
+            this.txtMaKH.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(217, 23);
+            this.txtMaKH.TabIndex = 6;
+            this.txtMaKH.TabStop = false;
+            // 
+            // timeTgTiem
+            // 
+            this.timeTgTiem.Enabled = false;
+            this.timeTgTiem.Location = new System.Drawing.Point(58, 257);
+            this.timeTgTiem.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.timeTgTiem.Name = "timeTgTiem";
+            this.timeTgTiem.Size = new System.Drawing.Size(217, 23);
+            this.timeTgTiem.TabIndex = 8;
+            // 
+            // btnXacNhan
+            // 
+            this.btnXacNhan.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnXacNhan.FlatAppearance.BorderSize = 0;
+            this.btnXacNhan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXacNhan.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnXacNhan.ForeColor = System.Drawing.Color.White;
+            this.btnXacNhan.Location = new System.Drawing.Point(112, 303);
+            this.btnXacNhan.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.Size = new System.Drawing.Size(108, 28);
+            this.btnXacNhan.TabIndex = 13;
+            this.btnXacNhan.Text = "Xác nhận";
+            this.btnXacNhan.UseVisualStyleBackColor = false;
+            this.btnXacNhan.Click += new System.EventHandler(this.btnXacNhan_Click);
+            // 
+            // chkDuocTiem
+            // 
+            this.chkDuocTiem.AutoSize = true;
+            this.chkDuocTiem.Location = new System.Drawing.Point(58, 156);
+            this.chkDuocTiem.Name = "chkDuocTiem";
+            this.chkDuocTiem.Size = new System.Drawing.Size(81, 19);
+            this.chkDuocTiem.TabIndex = 14;
+            this.chkDuocTiem.Text = "Được tiêm";
+            this.chkDuocTiem.UseVisualStyleBackColor = true;
+            // 
+            // chkDaTiem
+            // 
+            this.chkDaTiem.AutoSize = true;
+            this.chkDaTiem.Location = new System.Drawing.Point(58, 192);
+            this.chkDaTiem.Name = "chkDaTiem";
+            this.chkDaTiem.Size = new System.Drawing.Size(67, 19);
+            this.chkDaTiem.TabIndex = 15;
+            this.chkDaTiem.Text = "Đã tiêm";
+            this.chkDaTiem.UseVisualStyleBackColor = true;
+            // 
+            // capNhatPhieuDKTiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 349);
+            this.Controls.Add(this.chkDaTiem);
+            this.Controls.Add(this.chkDuocTiem);
+            this.Controls.Add(this.btnXacNhan);
+            this.Controls.Add(this.timeTgTiem);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.lblTgTiem);
+            this.Controls.Add(this.lblMaKH);
+            this.Controls.Add(this.titleCapNhat);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Name = "capNhatPhieuDKTiem";
+            this.Text = "Cập nhật phiếu đăng ký tiêm";
+            this.Load += new System.EventHandler(this.capNhatPhieuDKTiem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label titleCapNhat;
+        private Label lblMaKH;
+        private Label lblTgTiem;
+        private TextBox txtMaKH;
+        private DateTimePicker timeTgTiem;
+        private Button btnXacNhan;
+        private CheckBox chkDuocTiem;
+        private CheckBox chkDaTiem;
+    }
+}
---
+++ TiemChung/PTTKHTTT/traCuuPhieuDKTiem.cs
@@ -0,0 +1,91 @@
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PTTKHTTT
+{
+    public partial class traCuuPhieuDKTiem : Form
+    {
+        public static traCuuPhieuDKTiem instance;
+        SqlConnection sqlConn;
+        SqlCommand sqlCmd;
+
+        String str = "Data Source=MINHVOPC;Initial Catalog=TIEMNGUA;Integrated Security=True";
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+        DataTable dataTable = new DataTable();
+
+        public void loadData()
+        {
+            sqlCmd = sqlConn.CreateCommand();
+            sqlCmd.CommandText = "SELECT * FROM PHIEUDANGKYTIEMCHUNG";
+            dataAdapter.SelectCommand = sqlCmd;
+
+            dataTable.Clear();
+            dataAdapter.Fill(dataTable);
+            lsTiem.DataSource = dataTable;
+        }
+
+
+        public traCuuPhieuDKTiem()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        private void traCuuPhieuDKTiem_Load(object sender, EventArgs e)
+        {
+            sqlConn = new SqlConnection(str);
+            sqlConn.Open();
+            loadData();
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            try
+            {              
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM PHIEUDANGKYTIEMCHUNG WHERE MAKH=@MAKH", sqlConn))
+                {
+                    if (txtMaKH.Text != "") { 
+                        cmd.Parameters.AddWithValue("MAKH", txtMaKH.Text);
+                        dataAdapter.SelectCommand = cmd;
+                        dataTable.Clear();
+                        dataAdapter.Fill(dataTable);
+                        lsTiem.DataSource = dataTable;
+                    } else { loadData(); }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtMaKH_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)//enter
+                btnTimKiem.PerformClick();
+        }
+
+        private void lsTiem_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = lsTiem.CurrentRow.Index;
+            capNhatPhieuDKTiem f2 = new capNhatPhieuDKTiem();
+            f2.Show();
+
+            capNhatPhieuDKTiem.instance.maDK = lsTiem.Rows[i].Cells[0].Value.ToString();
+            capNhatPhieuDKTiem.instance.maKH.Text = lsTiem.Rows[i].Cells[1].Value.ToString();
+            
+            if (lsTiem.Rows[i].Cells[2].Value.ToString().Equals("True"))
+            {
+                capNhatPhieuDKTiem.instance.checkDuocTiem.Checked = true;
+            }
+
+            if (lsTiem.Rows[i].Cells[3].Value.ToString().Equals("True"))
+            {
+                capNhatPhieuDKTiem.instance.checkDaTiem.Checked = true;
+            }
+
+            capNhatPhieuDKTiem.instance.dateTiem.Text = Convert.ToDateTime(lsTiem.Rows[i].Cells[4].Value).ToString();
+            //System.Diagnostics.Debug.WriteLine(Convert.ToDateTime(lsTiem.Rows[i].Cells[4].Value));
+        }
+    }
+}
